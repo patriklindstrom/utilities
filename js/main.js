02649d0c5057fee3212c87cada0e8fc63648814f
Patrik Lindstrom <patriklindstrom@hotmail.com>
2014-02-01 20:02:21 +0100

    The Click Event works
    I can control the val of the text fiels trough the unlogical events om
    jquery and js.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+function Log2Soap(logtxt){
 
+return logtxt.toLowerCase();
+}
+
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $("#LogTxt").val("ARE YOU ALIVE")
+    $("#SoapMsg").val("Could I be dead?");
+   $( "#do-transform" ).click(function() {
+    console.log( "Commence Transform was clicked" );
+    $("#SoapMsg").val(Log2Soap( $("#LogTxt").val()));
+});
+
+
+});
